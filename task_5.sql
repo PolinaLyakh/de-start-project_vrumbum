7a8f33fb71f97d7b99669bd0f92d3285113545db
PolinaLyakh <70780136+PolinaLyakh@users.noreply.github.com>
2024-09-12 16:20:14 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,10 @@
 /*добавьте сюда запрос для решения задания 5*/
+SELECT b.brand_origin,
+       max((p.price / (100 - p.discount)) * 100) AS max,
+       min((p.price / (100 - p.discount)) * 100) AS min
+FROM car_shop.purchases p 
+INNER JOIN car_shop.models m 
+     ON p.model_id = m.id
+INNER JOIN car_shop.brands b 
+     ON m.brand_id = b.id
+GROUP BY b.brand_origin;
