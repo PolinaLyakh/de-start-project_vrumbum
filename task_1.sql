5a498ecf6e78a8b7e30027fe91431f636833f382
PolinaLyakh <70780136+PolinaLyakh@users.noreply.github.com>
2024-09-12 16:10:55 +0300

    Update task_1.sql

--- task_1.sql
+++ task_1.sql
@@ -1,1 +1,4 @@
 /*добавьте сюда запрос для решения задания 1*/
+SELECT (sum(CASE WHEN m.gasoline_consumption IS NULL THEN 1 ELSE 0 END) * 100)
+            / count(*) AS nulls_percentage_gasoline_consumption
+FROM car_shop.models m;
