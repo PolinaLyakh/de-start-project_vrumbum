413e6dce3b1dc4d4659641187006d9052945bc01
PolinaLyakh <70780136+PolinaLyakh@users.noreply.github.com>
2024-09-12 16:19:02 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,12 @@
 /*добавьте сюда запрос для решения задания 4*/
+SELECT c.client_name AS person, 
+       string_agg((b.brand_name || ' ' || m.model_name), ', ') AS cars 
+FROM car_shop.clients c 
+INNER JOIN car_shop.purchases p 
+      ON p.client_id = c.id 
+INNER JOIN car_shop.models m 
+      ON p.model_id = m.id 
+INNER JOIN car_shop.brands b 
+      ON m.brand_id = b.id 
+GROUP BY c.client_name
+ORDER BY c.client_name;
