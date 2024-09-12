01231ae65f26fffc25ad7a1ef14167eb34d5bbf1
PolinaLyakh <70780136+PolinaLyakh@users.noreply.github.com>
2024-09-12 18:17:41 +0300

    Update INITIAL.sql

--- INITIAL.sql
+++ INITIAL.sql
@@ -1,1 +1,17 @@
 /*сохраните в этом файле запросы для первоначальной загрузки данных - создание схемы raw_data и таблицы sales и наполнение их данными из csv файла*/
+-- Схема для исходной таблицы
+CREATE SCHEMA raw_data;
+
+-- Ненормализованная таблица
+CREATE TABLE raw_data.sales (
+id integer PRIMARY KEY,
+auto varchar,
+gasoline_consumption REAL,
+price NUMERIC,
+date date,
+person_name varchar,
+phone varchar,
+discount integer,
+brand_origin varchar
+);
+
