9f466baa8934c0ba079b11f1ff5ce2e600830ddc
PolinaLyakh <70780136+PolinaLyakh@users.noreply.github.com>
2024-09-12 16:16:07 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,8 @@
 /*добавьте сюда запрос для решения задания 3*/
+SELECT extract(MONTH FROM p.date_purch::date) AS MONTH,
+       to_char(extract(YEAR FROM p.date_purch::date), '9999') AS YEAR,
+       round(avg(p.price), 2) AS price_avg
+FROM car_shop.purchases p 
+WHERE extract(YEAR FROM p.date_purch::date) = 2022
+GROUP BY extract(YEAR FROM p.date_purch::date), extract(MONTH FROM p.date_purch::date)
+ORDER BY MONTH;
